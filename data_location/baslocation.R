6988e40f6140caab8eb87b9604d29404ff7456c9
Ryohei <trickhedgehog@gmail.com>
2015-07-26 18:15:30 +0900

    国土数値情報から撮ってきたバス停データをdata.frame型に変換する．
    data_mapに緯度と経度がID毎に入る

---
+++ data_location/baslocation.R
@@ -0,0 +1,33 @@
+# xmlを読み込む（テキストデータのままっぽい）
+doc <- xmlParse("P11-10_47-jgd-g.xml")
+
+# <gml:pos>というタグの付いているやつをタグごと持ってくる
+items <- getNodeSet(doc,"//gml:pos")
+
+
+## items_numにタグを外して代入（文字列形式）
+#pos_search <- function(x){
+#	a <- xmlValue(x[[1]])
+#	for(i in 2:length(x)){
+#		a <- append(a,xmlValue(x[[i]]))
+#	}
+#	return(a)
+#}
+#items_num <- pos_search(items)
+
+# 上の式と同義
+items_num <- sapply(items, function(x) xmlValue(x))
+
+# スペース区切りになっているので，分割して連番に
+items_number <- sapply(items_num, function(x) as.numeric(unlist(strsplit(x," "))))
+
+# 緯度と経度に分割していく
+latitudes <- items_number[seq(1,length(items_number),2)]
+longtudes <- items_number[seq(2,length(items_number),2)]
+
+# バス停留所のIDと緯度経度の関係
+data_map <- data.frame(LATITUDE=latitudes, LONGTUDE=longtudes)
+
+# 沖縄っぽい地図が見える
+#plot(latitudes,longtudes)
+#plot(data_map)
